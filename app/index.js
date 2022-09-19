811de03ad993b552c539147ac5c567bca2e36ed2
Austin <38254442+SpecifiesDev@users.noreply.github.com>
2022-09-19 17:13:07 -0400

    Push Version 1.0.0
    Add fully functional snapchat loading module.
    
    Add a notification system that allows the user to click on the notification, pulling the app up.
    
    Made to be lightweight and not take up a ton of sys memory.

---
+++ app/index.js
@@ -0,0 +1,86 @@
+// import our necessary libs
+const { app, BrowserWindow, screen, Notification } = require('electron');
+const path = require('path');
+
+let window;
+
+// keep a placeholder for notif already sent
+let notif_sent = false;
+
+// set our app id
+app.setAppUserModelId("Snapchat");
+
+// utility function to check if a string has a number in it
+const hasNumber = (str) => {
+    return /\d/.test(str);
+}
+
+// this is an interval to check the screen's title every 10 seconds.
+// basically, in function it will allow us to send push notifications over the user's
+// system
+const checkWindow = () => {
+    let title = window.getTitle();
+
+    // create our notification info
+    let options = {
+        title: 'Snapchat Message',
+        body: `You've received a notification from Snapchat.`,
+        icon: path.join(`${__dirname}/icons/icon.png`)
+    }
+
+    if(hasNumber(title)) {
+
+        if(!(notif_sent)) {
+
+            let notif = new Notification(options);
+
+            notif.show();
+
+            notif.on('click', (event, arg) => {
+                window.show();
+            })
+
+
+            notif_sent = true;
+
+        }
+
+    } else {
+        notif_sent = false;
+    }
+}
+
+// function to create the new window with our needed parameters
+const createWindow = () => {
+    const win = new BrowserWindow({
+        x: screen.width / 2,
+        y: screen.height / 2,
+        width: 1400,
+        height: 800
+    });
+
+    // set the windows icon
+    win.setIcon(path.join(`${__dirname}/icons/icon.png`));
+
+    win.loadURL('https://web.snapchat.com/', {
+        userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36' 
+        // here we set the user agent to "appear" as a browser
+    });
+
+    // remove the menu bar, it's ugly
+    win.setMenu(null);
+
+    return win;
+}
+
+
+// once the app is ready, load the window
+app.whenReady().then(() => {
+    window = createWindow();
+
+    // set the checkWindow interval
+    setInterval(() => {
+        checkWindow()
+    }, 1000);
+
+});
